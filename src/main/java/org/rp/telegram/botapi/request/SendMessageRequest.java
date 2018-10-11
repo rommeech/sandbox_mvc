552ba24073cdfa06bba0849ccb2854375d8d9774
Roman Parshin <parshin@smartmediastar.com>
2018-10-11 17:45:02 +0200

    SendMessageRequest.Builder added

--- src/main/java/org/rp/telegram/botapi/request/SendMessageRequest.java
+++ src/main/java/org/rp/telegram/botapi/request/SendMessageRequest.java
@@ -3,6 +3,7 @@ package org.rp.telegram.botapi.request;
 import org.rp.telegram.botapi.helper.FormatOption;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class SendMessageRequest implements Serializable {
     private static final long serialVersionUID = 7472599720120541333L;
@@ -28,8 +29,13 @@ public class SendMessageRequest implements Serializable {
     public SendMessageRequest() {
     }
 
-    public static SendMessageRequest getInstance() {
-        return new SendMessageRequest();
+    public SendMessageRequest(Builder builder) {
+        this.chatId = builder.chatId;
+        this.text = builder.text;
+        this.parseMode = builder.parseMode;
+        this.disableWebPagePreview = builder.disableWebPagePreview;
+        this.disableNotification = builder.disableNotification;
+        this.replyToMessageId = builder.replyToMessageId;
     }
 
     public String getChatId() {
@@ -78,5 +84,85 @@ public class SendMessageRequest implements Serializable {
 
     public void setReplyToMessageId(Integer replyToMessageId) {
         this.replyToMessageId = replyToMessageId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SendMessageRequest)) return false;
+        SendMessageRequest that = (SendMessageRequest) o;
+        return Objects.equals(chatId, that.chatId) &&
+                Objects.equals(text, that.text) &&
+                parseMode == that.parseMode &&
+                Objects.equals(disableWebPagePreview, that.disableWebPagePreview) &&
+                Objects.equals(disableNotification, that.disableNotification) &&
+                Objects.equals(replyToMessageId, that.replyToMessageId);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(chatId, text, parseMode, disableWebPagePreview, disableNotification, replyToMessageId);
+    }
+
+    @Override
+    public String toString() {
+        return "SendMessageRequest{" +
+                "chatId='" + chatId + '\'' +
+                ", text='" + text + '\'' +
+                ", parseMode=" + parseMode +
+                ", disableWebPagePreview=" + disableWebPagePreview +
+                ", disableNotification=" + disableNotification +
+                ", replyToMessageId=" + replyToMessageId +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String chatId;
+        private String text;
+        private FormatOption parseMode;
+        private Boolean disableWebPagePreview;
+        private Boolean disableNotification;
+        private Integer replyToMessageId;
+
+        public Builder() {
+
+        }
+
+        public Builder chatId(Integer chatId) {
+            this.chatId = chatId.toString();
+            return this;
+        }
+
+        public Builder text(String text) {
+            this.text = text;
+            return this;
+        }
+
+        public Builder parseMode(FormatOption parseMode) {
+            this.parseMode = parseMode;
+            return this;
+        }
+
+        public Builder disableWebPagePreview(Boolean disableWebPagePreview) {
+            this.disableWebPagePreview = disableWebPagePreview;
+            return this;
+        }
+
+        public Builder disableNotification(Boolean disableNotification) {
+            this.disableNotification = disableNotification;
+            return this;
+        }
+
+        public Builder replyToMessageId(Integer replyToMessageId) {
+            this.replyToMessageId = replyToMessageId;
+            return this;
+        }
+
+        public SendMessageRequest build() {
+            return new SendMessageRequest(this);
+        }
+
     }
 }
