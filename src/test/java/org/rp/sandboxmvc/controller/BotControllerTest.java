7c7d5e08f2b4d9fd7b8d9fe7dc373c736118d714
Roman Parshin <parshin@smartmediastar.com>
2018-11-09 15:55:17 +0100

    UnitTesting started

---
+++ src/test/java/org/rp/sandboxmvc/controller/BotControllerTest.java
@@ -0,0 +1,46 @@
+package org.rp.sandboxmvc.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.rp.sandboxmvc.model.Bot;
+import org.rp.sandboxmvc.service.BotService;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.ui.ExtendedModelMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BotControllerTest {
+
+    private final List<Bot> bots = new ArrayList<>();
+
+    @Before
+    public void setUp() throws Exception {
+        Bot bot = new Bot();
+        bot.setId(1L);
+        bot.setAbout("About");
+        bot.setToken(UUID.randomUUID().toString());
+        bot.setUserId(2);
+        bot.setUsername(UUID.randomUUID().toString());
+        bots.add(bot);
+    }
+
+    @Test
+    public void testBotList() {
+       BotService botService = mock(BotService.class);
+
+       BotController botController = new BotController();
+       ReflectionTestUtils.setField(botController, "botService", botService);
+
+        ExtendedModelMap uiModel = new ExtendedModelMap();
+        uiModel.addAttribute("bots", )
+
+    }
+
+
+}
